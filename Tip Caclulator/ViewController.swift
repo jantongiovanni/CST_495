7b69dd9956b5e15d0c1d62fc9ac1ee9dbe205729
Joseph Antongiovanni <anto1985@m506-118-26424.local>
2018-01-24 15:35:00 -0800

    Tip Calc first commit

--- Tip Caclulator/ViewController.swift
+++ Tip Caclulator/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,21 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func onTap(_ sender: AnyObject) {
+        view.endEditing(true)
+    }
+    
+    @IBAction func calculateTip(_ sender: AnyObject) {
+        let bill = Double(billField.text!) ?? 0
+        let tip = bill * 0.2
+        let total = bill + tip
+        //let is a variable i'm not going to change the name of later, taking the value inside of textfield and putting into bill
+        // ?? 0 handles non numeric values and returns default value 0
+        
+        tipLabel.text = "$\(tip)"
+        totalLabel.text = "$\(total)"
+        
+        // \(variable) anything within the parenthesis thats a variable change it into whatever its value is
+    }
 }
 
